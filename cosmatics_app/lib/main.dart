efe6974351c6818b558635110a43dc003efa1d92
weaamokok <47139547+weaamokok@users.noreply.github.com>
2023-06-06 10:22:42 +0200

    hsitory list setup

--- cosmatics_app/lib/main.dart
+++ cosmatics_app/lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:cosmatics_app/controllers/brands_controller.dart';
 import 'package:cosmatics_app/controllers/popular_products_controller.dart';
-import 'package:cosmatics_app/controllers/top_sale_controllers.dart';
 import 'package:cosmatics_app/pages/splash/splash_screen.dart';
 import 'package:cosmatics_app/route/route_helper.dart';
-import 'package:cosmatics_app/utils/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_translator/google_translator.dart';
 import 'controllers/cart_controller.dart';
 import 'helper/dependencies.dart' as dep;
 
